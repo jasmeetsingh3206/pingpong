bfc5f39616915d48f001cf4e3ac189a6dd0f83b7
sonarsarbjeet <sarbjeet@sonartechnologies.com>
2023-05-29 14:30:55 +0530

    more slow

--- client/loginPageViews/src/components/mainGame.vue
+++ client/loginPageViews/src/components/mainGame.vue
@@ -262,7 +262,7 @@ export default {
         }
       })
       document.addEventListener('keydown', (event) => {
-        if (this.pv < 10) this.pv = this.pv + 5;
+        if (this.pv < 6) this.pv = this.pv + 5;
         if (event.code === 'Space' && !this.showChat) this.socket.emit('space')
         if (event.code === 'ArrowLeft') {
           this.socket.emit('movePaddle', {
@@ -279,7 +279,7 @@ export default {
         }
       })
       document.addEventListener('keyup', () => {
-        this.pv=4;
+        this.pv=3;
       })
     }
   },
