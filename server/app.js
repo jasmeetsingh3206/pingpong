ad38be7662f43a95384a11930621266f0e4cdd1f
sonarsarbjeet <sarbjeet@sonartechnologies.com>
2023-05-30 07:35:43 +0530

    again fixed speed in server

--- server/app.js
+++ server/app.js
@@ -141,7 +141,7 @@ io.on("connection", (socket) => {
     socket.on("movePaddle", (data) => {
       
       if (numClients == 2) {
-        let speed =30;
+        let speed =data.pv;
         let count1 = 0;
         let count2 = 0;
         let count3 = 0;
