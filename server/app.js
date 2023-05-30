6919daad5c5f274f6f306c11d2e0c221f73bea9b
sonarsarbjeet <sarbjeet@sonartechnologies.com>
2023-05-30 08:05:23 +0530

    speed 18

--- server/app.js
+++ server/app.js
@@ -141,7 +141,7 @@ io.on("connection", (socket) => {
     socket.on("movePaddle", (data) => {
       
       if (numClients == 2) {
-        let speed =10;
+        let speed =18;
         let count1 = 0;
         let count2 = 0;
         let count3 = 0;
