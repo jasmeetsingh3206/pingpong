4dd7110dcd2ef317302f9f143114c77f75f78bbb
Utsav Sharma <utsavsharma1401@gmail.com>
2023-06-09 08:44:41 +0530

    final update

--- server/app.js
+++ server/app.js
@@ -343,7 +343,8 @@ console.log(roomdata[data.key].direction2 +'i am direction2')
                   }
 
                   roomdata[data.key].x_1 =
-                    roomdata[data.key].x_1 + roomdata[data.key].dx_1;
+                    roomdata[data.key].x_1 + roomdata[data.key].dx_1 -0.3;
+                   
                 }
               } else {
                 if (
